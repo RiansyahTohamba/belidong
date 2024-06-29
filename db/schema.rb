c9dab558945f8b374eedbb61c130a3dfc9649a3e
RiansyahTohamba <mriansyah93@gmail.com>
2024-06-29 11:48:47 +0800

    feat: init order model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_19_004847) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_29_034357) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "status"
+    t.decimal "total_price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.decimal "price"
