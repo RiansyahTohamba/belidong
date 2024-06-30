dc30abd09570b4716ad7eb695022fbc84f8416df
RiansyahTohamba <mriansyah93@gmail.com>
2024-06-30 16:26:28 +0800

    feat: add merchant

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_29_034357) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_29_215225) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "merchants", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
